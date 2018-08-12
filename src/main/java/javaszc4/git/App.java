e5138fcd3e4dab70dcf65b4286f532c1934eb900
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-08-12 10:31:23 +0200

    Dodanie wyświetlanie tekstóœ

---
+++ src/main/java/javaszc4/git/App.java
@@ -0,0 +1,17 @@
+package javaszc4.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void wyswietlTekst(String tekst) {
+        System.out.println(String.format("Tekst: {0}", tekst));
+    }
+}
