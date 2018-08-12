dd6e0ab51ef2729cb4698ec37530ccf7c49f72f1
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-08-12 10:45:56 +0200

    Dodanie sumowania tablicy i testów

--- src/test/java/javaszc4/git/AppTest.java
+++ src/test/java/javaszc4/git/AppTest.java
@@ -1,20 +1,31 @@
 package javaszc4.git;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void wyswietlTekst() {
+        App.wyswietlTekst("Test");
+    }
+
+    @Test
+    public void testSumowanieTablicy() {
+        int[] tablica = {3, 4, 5};
+        int suma = App.sumowanieTablicy(tablica);
+        assertEquals(suma, 3 + 4 + 5);
     }
 }
