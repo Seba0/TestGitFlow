ee85759b86722ca71e2f8a9b9debe02c26ff7f4d
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-08-12 13:12:31 +0200

    Dodanie testów

--- test-maven-project/src/main/java/com/seba0/sda/App.java
+++ test-maven-project/src/main/java/com/seba0/sda/App.java
@@ -2,12 +2,24 @@ package com.seba0.sda;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static void wyswietlTekst(String tekst) {
+        System.out.println(String.format("Tekst: {0}", tekst));
+    }
+
+    public static int sumowanieTablicy(int[] tablica) {
+        if (tablica == null) {
+            return 0;
+        }
+        int suma = 0;
+        for (int element : tablica) {
+            suma += element;
+        }
+        return suma;
     }
 }
--- test-maven-project/src/test/java/com/seba0/sda/AppTest.java
+++ test-maven-project/src/test/java/com/seba0/sda/AppTest.java
@@ -2,6 +2,9 @@ package com.seba0.sda;
 
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 /**
@@ -14,5 +17,32 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void wyswietlTekst() {
+        App.wyswietlTekst("Test");
+    }
+
+    @Test
+    public void testSumowanieTablicy345() {
+        //given
+        int[] tablica = {3, 4, 5};
+        System.out.println("Sumowanie tablicy: " + Arrays.toString(tablica));
+        int suma = App.sumowanieTablicy(tablica);
+        System.out.println("Suma: " + suma);
+        assertEquals(suma, 3 + 4 + 5);
+    }
+
+    @Test
+    public void testSumowanieTablicyNull() {
+        //given
+        int[] tablica = null;
+        System.out.println("Sumowanie tablicy: " + Arrays.toString(tablica));
+        //when
+        int suma = App.sumowanieTablicy(tablica);
+        System.out.println("Suma: " + suma);
+        //then
+        assertEquals(suma, 0);
     }
 }
