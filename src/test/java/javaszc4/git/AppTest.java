9bf59b75bab35b929b75bfaf044c28f923a78481
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-08-12 10:54:50 +0200

    Poprawienie testu

--- src/test/java/javaszc4/git/AppTest.java
+++ src/test/java/javaszc4/git/AppTest.java
@@ -2,6 +2,8 @@ package javaszc4.git;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -25,7 +27,9 @@ public class AppTest {
     @Test
     public void testSumowanieTablicy() {
         int[] tablica = {3, 4, 5};
+        System.out.println("Sumowanie tablicy: " + Arrays.toString(tablica));
         int suma = App.sumowanieTablicy(tablica);
+        System.out.println("Suma: " + suma);
         assertEquals(suma, 3 + 4 + 5);
     }
 }
